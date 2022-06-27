deed3d5329d9207deb3f7dc27e126525282337d8
Qasem Talaee <qasem.talaee1375@gmail.com>
2022-06-27 14:48:15 +0430

    try to fix

--- lib/request_client_spot.py
+++ lib/request_client_spot.py
@@ -48,6 +48,7 @@ class RequestClient(object):
         data['tonce'] = int(time.time()*1000)
         self.get_auth(data)
         response = requests.post(url, headers=self.headers, params=data)
+        print(response.text)
         if response.status_code == requests.codes.ok:
             if response.json()['code'] == 0:
                 return response.json()
