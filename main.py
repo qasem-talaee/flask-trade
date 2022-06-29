a0e847ebc17f49cd27733586a60817adbff3a6ca
Qasem Talaee <qasem.talaee1375@gmail.com>
2022-06-29 15:20:25 +0430

    fix in session main

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from lib import db
 from lib import api
 
 app = Flask(__name__)
-app.config["SESSION_PERMANENT"] = True
+app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 socketio = SocketIO(app)
